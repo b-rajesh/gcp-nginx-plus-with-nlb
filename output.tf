8fba1ef52fdfe0d7af68caba86ec809b247514b5
Rajesh Bavananatham <rajesh.bavan@gmail.com>
2020-10-07 18:03:21 +1100

    Fixed output

--- output.tf
+++ output.tf
@@ -1,16 +1,16 @@
 output "weather_api_url" {
   description = "Curl command to access weather API"
-  value       = "curl http://${google_compute_forwarding_rule.gce-ext-lb-80-forwarding-rule.ip_address}/weather?city=melbourne"
+  value       = "curl http://${google_compute_forwarding_rule.gce-ext-lb-80-forwarding-rule.ip_address}/weather?city=melbourne -H 'Authorization: Bearer ' "
 }
 
 output "hello-nginxplus_api_url" {
   description = "HTTPie command to access Hellow NGINX Plus API"
-  value       = "http ${google_compute_forwarding_rule.gce-ext-lb-80-forwarding-rule.ip_address}/hello-nginxplus-api"
+  value       = "http ${google_compute_forwarding_rule.gce-ext-lb-80-forwarding-rule.ip_address}/hello-nginxplus-api 'Authorization: Bearer '"
 }
 
 output "f1_api_url" {
   description = "HTTPie command to access F1 detail through API"
-  value       = "http ${google_compute_forwarding_rule.gce-ext-lb-80-forwarding-rule.ip_address}/f1-api/f1/drivers.json"
+  value       = "http ${google_compute_forwarding_rule.gce-ext-lb-80-forwarding-rule.ip_address}/f1-api/f1/drivers.json 'Authorization: Bearer '"
 }
 
 output "nginxplus_dashboard_url" {
@@ -21,14 +21,4 @@ output "nginxplus_dashboard_url" {
 output "admin_api_url" {
   description = "HTTPie command to access Admin API"
   value       = "http ${google_compute_forwarding_rule.gce-ext-lb-80-forwarding-rule.ip_address}:8080/api/6/http/upstreams"
-}
-
-output "inventory_api_url" {
-  description = "HTTPie command to access Inventory API"
-  value       = "http ${google_compute_forwarding_rule.gce-ext-lb-80-forwarding-rule.ip_address}/warehouse-api/inventory"
-}
-
-output "pricing_api_url" {
-  description = "HTTPie command to access Pricing API"
-  value       = "http ${google_compute_forwarding_rule.gce-ext-lb-80-forwarding-rule.ip_address}/warehouse-api/pricing"
 }
